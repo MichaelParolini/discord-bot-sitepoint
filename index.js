d437d8c709715368e480e8ad27f9fd9884a3a98d
Michael Parolini <81749955+MichaelParolini@users.noreply.github.com>
2021-04-23 16:26:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,64 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => 
-{
+bot.on('message', msg => {
+  
   if (msg.content === 'ShowMap' || msg.content === 'uscita')
    {
     msg.channel.send('map', {files: ["https://i.imgur.com/UDt2JJS.jpg"]});
    }
    
 });
+
+
+//HELP
+  //Interchange
+    //exits
+    //stashes
+    //keys
+    //Boss
+
+  //Woods
+    //exits
+    //stashes
+    //keys
+    //Boss
+    
+  //Customs
+    //exits
+    //stashes
+    //keys
+    //Boss
+
+  //Labs
+    //exits
+    //stashes
+    //keys
+    //Boss
+
+  //Shoreline
+    //exits
+    //stashes
+    //keys
+    //Boss
+
+  //Reserve
+    //exits
+    //stashes
+    //keys
+    //Boss  
+
+
+  
+  
+  //Bullets
+  //Gear
+  //Traders
+  //Keys
+  //Backpacks
+
+
+
+
+  //BUILDS
+    //weapons
